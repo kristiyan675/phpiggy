96b79039c0a8cfe3285451a538f100c338fe6f49
kristiyan675 <vasilev.kristiyan.v@gmail.com>
2023-12-18 14:21:52 +0200

    routing init

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace Framework;
 
+
 class App {
+    private Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+
     public function run () {
-        echo "Application running";
+        $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+        $method = $_SERVER["REQUEST_METHOD"];
+        $this->router->dispatch($path, $method);
+    }
+
+    public function get(string $path, $controller){
+        $this->router->add('GET', $path, $controller);
     }
 }
